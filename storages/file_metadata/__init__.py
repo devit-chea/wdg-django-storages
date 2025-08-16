4d23c1a236a05e83ef4fd2b8fb28069fa9e87c8b
devit-chea <devit.chea1998@gmail.com>
2025-08-16 13:07:29 +0700

    Add init file for file metadata

---
+++ storages/file_metadata/__init__.py
@@ -0,0 +1,5 @@
+"""
+ChartGPT has detected recent edits in the following files. The edits are related to pre-signed URLs and multipart upload support in a Django storage application.
+The files include:
+Ref: https://chatgpt.com/share/68a01bbc-9790-8013-8d4b-797f997aa659
+"""
